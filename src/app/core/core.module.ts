64c8216e2ff715cd1ed582cb177c863c2c23c046
CEIBA\alejandro.jaramillo <alejandro.jaramillo@ceiba.com.co>
2022-02-01 21:49:52 -0500

    Estructura inicial del proyecto en angular

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './component/navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
